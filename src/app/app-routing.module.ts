6b7d3eceef4412e1950956f3a998a94223e1ba6a
ravinduDon <ravindugunarathna@gmail.com>
2020-05-16 17:24:45 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {EducationComponent} from './education/education.component';
+import {ExperianceComponent} from './experiance/experiance.component';
+import {ContactComponent} from './contact/contact.component';
+import {LanguagesComponent} from './child/languages/languages.component';
+import {ProjectsComponent} from './child/projects/projects.component';
+import {SkillsComponent} from './child/skills/skills.component';
+import {TechnologiesComponent} from './child/technologies/technologies.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/about'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'education',
+    component: EducationComponent
+  },
+  {
+    path: 'experiance',
+    component: ExperianceComponent,
+    children: [
+      {path: 'languages', component: LanguagesComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'skills', component: SkillsComponent},
+      {path: 'technology', component: TechnologiesComponent},
+      {path: '', pathMatch: 'full', redirectTo: '/skills'},
+    ]
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
