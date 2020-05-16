b15a108fb7e39751d8f02c43280c38121e349ded
ravinduDon <ravindugunarathna@gmail.com>
2020-05-16 13:46:21 +0530

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ExperianceComponent } from './experiance/experiance.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './child/skills/skills.component';
+import { ProjectsComponent } from './child/projects/projects.component';
+import { TechnologiesComponent } from './child/technologies/technologies.component';
+import { LanguagesComponent } from './child/languages/languages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperianceComponent,
+    ContactComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    TechnologiesComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/experiance/experiance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiance',
+  templateUrl: './experiance.component.html',
+  styleUrls: ['./experiance.component.css']
+})
+export class ExperianceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
